458298e95ddadfc59e6248986ad50c1ac79f995c
[jo] <jonespjunior@gmail.com>
2023-08-23 08:19:20 -0400

    cosnegui colocar em tabelas, falta só alinhar

--- JS1/JS1.js
+++ JS1/JS1.js
@@ -1,14 +1,13 @@
+document.write('<table border="1">');
 for (let i=1; i <= 10; i++) {
-    document.write("Produtos de ");
-    document.write(i);
-    document.write("<br>");
+    document.write('<tr>');
+    document.write('<th colspan="2">Produtos de ' + i + '</th>');
+    document.write('</tr>');
     for (let j=1; j <= 10; j++) {
-        document.write(i);
-        document.write("x");
-        document.write(j);
-        document.write(" = ");
-        document.write(i*j);
-        document.write("<br>");
+        document.write('<tr>');
+        document.write('<td>' + i +'x'+j+'</td>');
+        document.write('<td>' + (i * j) + '</td>');
+        document.write('</tr>');
     }
-    document.write("<br>");
 }
+document.write('</table>');
