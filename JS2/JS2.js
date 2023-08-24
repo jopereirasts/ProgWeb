ab622208f45999fb291281830381f025b3436a50
[jo] <jonespjunior@gmail.com>
2023-08-24 11:25:54 -0400

    comecei, ainda tentando

---
+++ JS2/JS2.js
@@ -0,0 +1,8 @@
+console.log("Escolha sua jogada:");
+console.log("1 - Papel");
+console.log("2 - Pedra");
+console.log("3 - Tesoura");
+
+//let jogada = parseInt(prompt());
+let pc = Math.floor(Math.random() * (3 - 1) + 1);
+console.log(pc);
