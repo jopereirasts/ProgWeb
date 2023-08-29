b86e4b513857c403cd258839d59ae8d1ad0ba2aa
[jo] <jonespjunior@gmail.com>
2023-08-29 14:33:52 -0400

    funções de conjunto

--- JS3/JS3.js
+++ JS3/JS3.js
@@ -15,4 +15,28 @@ class IntegerSet {
             this.set[element] = false;
         }
     }
+
+    union(otherSet) {
+        const newSet = new IntegerSet(this.maxValue);
+        for (let i = 0; i <= this.maxValue; i++) {
+          newSet.set[i] = this.set[i] || otherSet.set[i];
+        }
+        return newSet;
+      }
+    
+    intersection(otherSet) {
+        const newSet = new IntegerSet(this.maxValue);
+        for (let i = 0; i <= this.maxValue; i++) {
+            newSet.set[i] = this.set[i] && otherSet.set[i];
+        }
+        return newSet;
+    }
+    
+    difference(otherSet) {
+        const newSet = new IntegerSet(this.maxValue);
+        for (let i = 0; i <= this.maxValue; i++) {
+          newSet.set[i] = this.set[i] && !otherSet.set[i];
+        }
+        return newSet;
+    }
 }
