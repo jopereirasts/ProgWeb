f23c8191d0afd0a4a1df4cec3366069152bf273c
[jo] <jonespjunior@gmail.com>
2023-08-29 14:22:51 -0400

    insert e remove colocados

--- JS3/JS3.js
+++ JS3/JS3.js
@@ -3,4 +3,16 @@ class IntegerSet {
       this.maxValue = maxValue;
       this.set = new Array(maxValue + 1).fill(false);
     }
+
+    insert(element) {
+        if (element >= 0 && element <= this.maxValue) {
+          this.set[element] = true;
+        }
+      }
+    
+    remove(element) {
+        if (element >= 0 && element <= this.maxValue) {
+            this.set[element] = false;
+        }
+    }
 }
