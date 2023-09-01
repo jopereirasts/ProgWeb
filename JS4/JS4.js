944d1493217984664fa9a764be1fa473bce692d4
[jo] <jonespjunior@gmail.com>
2023-09-01 18:58:58 -0400

    coloquei em funções e terminei

--- JS4/JS4.js
+++ JS4/JS4.js
@@ -1,3 +1,14 @@
-let raio = document.myForm.raio.value;
-let area = Math.PI * (raio*raio);
-let circ = (Math.PI*raio)*2;
+function calcula() {
+    let raio = parseFloat(document.myForm.raio.value); // Parse the input value as a number
+
+    let area = Math.PI * (raio * raio);
+    let circ = 2 * Math.PI * raio;
+
+    document.myForm.area.value = area.toFixed(2);
+    document.myForm.circ.value = circ.toFixed(2);
+}
+
+document.myForm.addEventListener('submit', function (e) {
+    e.preventDefault(); // Impedir o envio do formulário
+    calcula(); // Chamar a função calculate
+});
