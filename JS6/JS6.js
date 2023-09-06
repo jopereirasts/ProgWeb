992ca4a8db25383638ee847b5fcfcb7f647f56f5
[jo] <jonespjunior@gmail.com>
2023-09-05 20:07:28 -0400

    não tava indo, agora foi

--- JS6/JS6.js
+++ JS6/JS6.js
@@ -1,8 +1,21 @@
-let maximo = 8;
-let pontinhos = [];
+const maximo = 8;
+const pontinhos = [];
 
-document.addEventListener("mousemove",function(e){
-    let ponto = document.createElement("div");
-    document.body.appendChild(ponto);
-    pontinhos.push(ponto);
+document.addEventListener("mousemove", (e) => {
+  if (pontinhos.length >= maximo) {
+    const pontoRemovido = pontinhos.shift();
+    pontoRemovido.remove();
+  }
+
+  const ponto = document.createElement("div");
+  ponto.className = "ponto";
+  document.body.appendChild(ponto);
+
+  const tamanhoPonto = 20; // Tamanho do ponto
+  ponto.style.width = `${tamanhoPonto}px`;
+  ponto.style.height = `${tamanhoPonto}px`;
+  ponto.style.left = `${e.clientX - tamanhoPonto / 2}px`;
+  ponto.style.top = `${e.clientY - tamanhoPonto / 2}px`;
+
+  pontinhos.push(ponto);
 });
