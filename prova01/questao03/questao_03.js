100065849f24c1ffa959a0a79c365318c6f0ef73
Jo Pereira <70331730+jopereirasts@users.noreply.github.com>
2023-09-13 17:15:02 -0400

    Create questao_03.js

---
+++ prova01/questao03/questao_03.js
@@ -0,0 +1,42 @@
+class Venda{
+    constructor(id,qtd,preco){
+        this.id = id;
+        this.qtd = qtd
+        this.preco = preco;
+    }
+
+    getId(){
+        return this.id;
+    }
+
+    getQtd(){
+        return this.qtd;
+    }
+
+    getPreco(){
+        return this.preco;
+    }
+
+    setId(id){
+        this.id = id;
+    }
+
+    setQtd(qtd){
+        this.qtd = qtd;
+    }
+
+    setPreco(preco){
+        this.preco = preco;
+    }
+
+    getValorTotal(){
+        return this.getPreco()*this.getQtd();
+    }
+}
+
+const venda1 = new Venda(1,2,3);
+const venda2 = new Venda(2,4,2);
+let total1 = venda1.getValorTotal();
+let total2 = venda2.getValorTotal();
+console.log(`Total de venda da venda de id ${venda1.id}: ${total1}`);
+console.log(`Total de venda da venda de id ${venda2.id}: ${total2}`);
