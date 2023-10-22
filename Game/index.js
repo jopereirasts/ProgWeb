da5cc49336a26c26636d5c5067805340cde67c42
[jo] <jonespjunior@gmail.com>
2023-10-22 18:18:45 -0400

    consegui colocar icones

--- Game/index.js
+++ Game/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = require('./src/router/router');
 const handlebars = require("express-handlebars");
+const sass = require('node-sass-middleware');
 
 const morgan = require('morgan');
 
@@ -8,8 +9,16 @@ const app = express()
 const PORT = 4455;
 
 app.use(router)
-app.use("/img", express.static(`${__dirname}/public/img`)); 
 app.use(morgan("combined"));
+
+app.use("/img", express.static(`${__dirname}/public/img`)); 
+app.use("/css", express.static(`${__dirname}/public/css`)); 
+app.use("/webfonts", express.static(`${__dirname}/node_modules/@fortawesome/fontawesome-free/webfonts`)); 
+app.use('/js',[
+    express.static(`${__dirname}/public/js`),
+    express.static(`${__dirname}/node_modules/bootstrap/dist/js/`)
+
+]);
 
 app.engine("handlebars",handlebars.engine({
     helpers: require(`${__dirname}/src/views/helpers/helpers`),
@@ -18,6 +27,13 @@ app.engine("handlebars",handlebars.engine({
 }));
 app.set("view engine","handlebars");
 app.set("views", `${__dirname}/src/views`);
+
+app.use(sass({
+    src: `${__dirname}/public/scss`,
+    dest: `${__dirname}/public/css`,
+    outputStyle: "compressed",
+    prefix: "/css",
+}));
 
 app.listen(PORT, ()=>{
     console.log(`Escutando na porta ${PORT}!`);
