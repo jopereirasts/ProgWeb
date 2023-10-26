246ac626f13f59a02c18e7dfe20e018145ab5dc6
[jo] <jonespjunior@gmail.com>
2023-10-26 15:33:59 -0400

    retornei o codigo antigo

--- Game/app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const PORT = 3000;
-app.get("/", (req, res) => {
-res.send("Hello world!");
-});
-app.listen(PORT, () => {
-console.log(`Express app iniciada na porta ${PORT}.`);
-});
---
+++ Game/src/router/router.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const mainController = require("../controllers/main");
+const router = express.Router();
+
+router.get("/", mainController.index);
+router.get("/about", mainController.about);
+router.get("/profs", mainController.profs);
+router.get("/ui", mainController.ui);
+
+module.exports = router;
