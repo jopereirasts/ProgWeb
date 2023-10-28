4aee12012f81b2c38e89804a4e11d6a4f0218b85
[jo] <jonespjunior@gmail.com>
2023-10-28 00:41:27 -0400

    criei a rota area

--- Game/src/router/router.js
+++ Game/src/router/router.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const mainController = require("../controllers/main");
+const AreaController = require("../controllers/area");
 const router = express.Router();
 
+//MainController
 router.get("/", mainController.index);
 router.get("/about", mainController.about);
 router.get("/ui", mainController.ui);
 router.get("/game", mainController.game);
+
+//AreaController
+router.get("/area",AreaController.index);
+
 
 module.exports = router;
