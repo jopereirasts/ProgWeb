fc298335d6ae816432c940695dba510ba59292a1
[jo] <jonespjunior@gmail.com>
2023-10-28 21:23:06 -0400

    criei as migrations de associacoes (ex. 8)

---
+++ Game/src/database/migrations/20231029005642-relacoes_usuario.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addConstraint('Usuarios', {
+      type: 'foreign key',
+      fields: ['cursoId'],
+      name: 'usuario_curso_fk',
+      references: {
+        table: 'Cursos',
+        field: 'id'
+      },
+      onDelete: 'restrict',
+      onUpdate: 'restrict'
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeConstraint(
+      'Usuarios',
+      'usuario_curso_fk'
+    )
+  }
+};
